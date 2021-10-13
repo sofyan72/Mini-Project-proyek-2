162e221ff925e9caa90301c1a12f487ed0449aa2
Ahmad Sofyan .A <sofyan7273@gmail.com>
2021-10-13 09:13:01 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('pesan/{id}', [App\Http\Controllers\PesanController::class, 'index'])->name('pesan');
+Route::post('pesan/{id}', [App\Http\Controllers\PesanController::class, 'pesan'])->name('pesan');
+
+Route::get('check-out', [App\Http\Controllers\PesanController::class, 'check_out'])->name('checkout');
+Route::delete('check-out/{id}', [App\Http\Controllers\PesanController::class, 'delete'])->name('checkout');
+
+Route::get('konfirmasi-check-out', [App\Http\Controllers\PesanController::class, 'konfirmasi'])->name('checkout');
+
+Route::get('profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
+Route::post('profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile');
+
+Route::get('history', [App\Http\Controllers\HistoryController::class, 'index'])->name('history');
+Route::get('history/{id}', [App\Http\Controllers\HistoryController::class, 'detail'])->name('history');
